ae27e6f1c167816125ac512fdce9c24364c4b463
FilWisher <williamfisher@hotmail.co.uk>
2016-04-04 10:40:01 +0100

    removes unused code

--- topology.js
+++ topology.js
@@ -23,35 +23,6 @@ function topology (nodes, edges) {
     }
     handlers[name] = fn
   }
-  /*
-    switch (ev.type) {
-      case 'request':
-        var node = get_node(ev.node, t.nodes)
-        if (!node) return
-        node.requests.push({
-          id: ev.data_ID
-        , loc: ev.node
-        })
-        
-        break
-      case 'request_hop':
-        var src = get_node(ev.from_node, t.nodes)
-        if (!src) return
-       
-        src.requests = src.requests.filter(function (r) {
-          return r.id !== ev.data_ID
-        })
-        
-        var dst = get_node(ev.to_node, t.nodes)
-        if (!dst) return
-        dst.requests.push({
-          id: ev.data_ID
-        , loc: ev.to_node
-        })
-        
-        break
-    }
-  }
-  */
+  
   return t
 }
