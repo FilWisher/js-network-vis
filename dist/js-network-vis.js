d1804e9d676ccde61b62630ff444a86f3f3addd4
FilWisher <williamfisher@hotmail.co.uk>
2016-04-04 10:41:38 +0100

    updates build

--- dist/js-network-vis.js
+++ dist/js-network-vis.js
@@ -43,7 +43,9 @@ module.exports = function (nodes, edges, opts) {
       return size
     }
   }
-  
+
+  network.event = network.graph.event
+
   network.canvas = d3.select(opts.element).append('svg')
     .attr('width', opts.width)
     .attr('height', opts.height)
@@ -9660,51 +9662,29 @@ module.exports = topology
 
 function topology (nodes, edges) {
   var t = {}
+  var handlers = {} 
   
   t.edges = edges
   t.nodes = nodes.map(function (node) {
     node.requests = []
     return node
   })
-  
+   
   t.update = function update(ev) {
 
-    switch (ev.type) {
-      case 'request':
-        var node = get_node(ev.node, t.nodes)
-        if (!node) return
-        node.requests.push({
-          id: ev.data_ID
-        , loc: ev.node
-        })
-        
-        break
-      case 'request_hop':
-        var src = get_node(ev.from_node, t.nodes)
-        if (!src) return
-       
-        src.requests = src.requests.filter(function (r) {
-          return r.id !== ev.data_ID
-        })
-        
-        var dst = get_node(ev.to_node, t.nodes)
-        if (!dst) return
-        dst.requests.push({
-          id: ev.data_ID
-        , loc: ev.to_node
-        })
-        
-        break
+    var h = handlers[ev.type]
+    if (!h) return
+    return h(ev, t.nodes, t.edges)
+  }
+  
+  t.event = function (name, fn) {
+    if (!name || !fn || typeof fn !== 'function') {
+      throw new Error('must register event-type with function')
     }
+    handlers[name] = fn
   }
   
   return t
-}
-
-function get_node(id, nodes) {
-  return nodes.filter(function (node) {
-    return node.name === id 
-  })[0]
 }
 
 },{}]},{},[1]);
